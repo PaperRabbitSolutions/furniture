975708433a37e3bf11b97560d9c0fd8a1d10eb8e
PaperRabbit <developpaperrabbit.world>
2024-09-11 11:09:10 +0530

    my commit

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ setMessage(false);
   <div className='w-screen h-screen bg-cover relative flex justify-center items-center bg-center bg-no-repeat' style={{backgroundImage:`url("https://png.pngtree.com/background/20231016/original/pngtree-red-and-black-frames-accent-a-sleek-dark-interior-with-an-picture-image_5582346.jpg")`}}>
 
   <div className='lg:w-[18%] md:w-[40%] md:h-[40%] lg:h-[45%] w-[60%] h-[50%] object-cover object-center rounded-xl absolute ml-10 mb-28 bg-transparent justify-center items-center flex-col gap-6  flex text-white'> 
-  <img src={star}  className='mb-[40%] w-[50%]' alt="" />
+  <img src={star}  className='mb-[45%] w-[50%]' alt="" />
   <h2 className='lg:text-3xl md:text-2xl text-xl text-white'>We Will be back soon</h2>
   <form action="" className='w-full flex flex-col gap-2 justify-center items-center'>
     <input className='w-[90%] px-2 py-2 rounded-lg bg-transparent text-white text-md placeholder:text-white outline-red-600 outline ' type="text" placeholder='Write your email' />
